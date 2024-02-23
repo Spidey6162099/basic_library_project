51cbae594477778e08966ca6d2fcc5d1e910d04d
Shaunak <141052710+Spidey6162099@users.noreply.github.com>
2024-02-23 14:03:29 +0530

    base changes completed

--- script.js
+++ script.js
@@ -1,137 +1,204 @@
-let library_book=[]
 
-function Book(title,author){//,pages ,read){
-    this.title=title;
-    this.author=author;
-    // this.pages=pages;
-    // this.read=read;
-    this.info=function(){
-        let read_or_not=!(this.read)?"not read":"read";
-        return `${this.title} by ${this.author} is ${this.pages} pages, and ${read_or_not}`;
+
+
+
+
+let book_list=[]
+
+class book{
+    
+    constructor(title,author,read,pages){
+        this.title=title;
+        this.author=author;
+        this.read=read;
+        this.pages=pages;
+    }
+
+}
+
+//function to add books
+function add(){
+    const book_name_value=document.querySelector(".user_input>form>p>#book_name");
+    const author_name_value=document.querySelector(".user_input>form>p>#author_name");
+    const pages_read_value=document.querySelector(".user_input>form>p>#page_no");
+    const read_value=document.querySelector(".user_input>form>p>#read_status");
+
+   
+
+    
+    
+    let temp=new book(book_name_value.value,author_name_value.value,read_value.checked,pages_read_value.value);
+    book_list.push(temp);
+    render();
+}
+
+//function to implement input validation
+
+function check(){
+    const book_name_value=document.querySelector(".user_input>form>p>#book_name");
+    const author_name_value=document.querySelector(".user_input>form>p>#author_name");
+    const pages_read_value=document.querySelector(".user_input>form>p>#page_no");
+    const read_value=document.querySelector(".user_input>form>p>#read_status");
+    let flag=true;
+    if(!book_name_value.value){
+        const error_msg=document.querySelector(".book_error");
+        error_msg.textContent="enter the damn book!"
+        error_msg.setAttribute("style","color:red");
+        flag=false;
+    }
+    else{
+        const error_msg=document.querySelector(".book_error");
+        error_msg.textContent="";
+    }
+    if(!author_name_value.value){
+        const error_msg=document.querySelector(".author_error");
+        error_msg.textContent="enter the damn author!"
+        error_msg.setAttribute("style","color:red");
+        flag=false;
+    }
+    else{
+        const error_msg=document.querySelector(".author_error");
+        error_msg.textContent="";
+    }
+    if(!pages_read_value.value){
+        const error_msg=document.querySelector(".page_error");
+        error_msg.textContent="enter the pages?"
+        error_msg.setAttribute("style","color:red");
+        flag=false;
+    }
+    else{
+        const error_msg=document.querySelector(".page_error");
+        error_msg.textContent="";
+    }
+    return flag;
+}
+
+//to remove errors and inputs
+function clear(){
+
+    const error_msg1=document.querySelector(".book_error");
+    error_msg1.textContent="";
+    const error_msg2=document.querySelector(".author_error");
+    error_msg2.textContent="";
+    const error_msg3=document.querySelector(".page_error");
+    error_msg3.textContent="";
+    const error_msg4=document.querySelector(".read_error");
+    error_msg4.textContent="";
+
+    document.querySelector(".user_input>form>p>#book_name").value="";
+    document.querySelector(".user_input>form>p>#author_name").value="";
+    document.querySelector(".user_input>form>p>#page_no").value="";
+    document.querySelector(".user_input>form>p>#read_status").checked=false;
+
+}
+
+function remove(e){
+    let index=Number(e.target.getAttribute("data_index"));
+    book_list.splice(index,1);
+    render();
+}
+
+function render(){
+    const container=document.querySelector(".body_container>.container");
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    let count=0;
+    for(let i of book_list){
+
+        // create cards in container
+       let box= document.createElement("div");
+       box.setAttribute("class","container_card");
+       
+       const book_name=document.createElement("div");
+       book_name.setAttribute("class","book_name");
+       book_name.textContent=i.title;
+       box.appendChild(book_name);
+       
+       const author_name=document.createElement("div");
+       author_name.setAttribute("class","author_name");
+       author_name.textContent=i.author;
+       box.appendChild(author_name);
+       
+       const icons=document.createElement("div");
+       icons.setAttribute("class","icons");
+
+       const read_btn=document.createElement("button");
+       read_btn.setAttribute("class","read");
+       read_btn.addEventListener("click",(e)=>{
+        i.read=!i.read;
+        render();
+       })
+       if(i.read){
+            read_btn.setAttribute("style","background-color:green");
+            read_btn.textContent="read";
+       }
+       else{
+        read_btn.setAttribute("style","background-color:red");
+        read_btn.textContent="unread";
+       }
+       
+       const remove_btn=document.createElement("button");
+       remove_btn.setAttribute("class","remove");
+       remove_btn.setAttribute("data_index",`${count}`);
+       remove_btn.addEventListener("click",remove);
+       remove_btn.textContent="remove";
+       
+       icons.appendChild(read_btn);
+       icons.appendChild(remove_btn);
+
+       box.appendChild(icons);
+
+       container.appendChild(box);
+       count++;
+
     }
 }
 
-//already added books
+function set_stage(){
 
-let lord_of_the_rings=new Book("Lord of the Rings","JRR Tolkien");
-library_book.push(lord_of_the_rings);
-let chronicles_of_narnia=new Book("Chronicles of Narnia","CS Lewis");
-library_book.push(chronicles_of_narnia);
-let don_quixote=new Book("Don Quixote","Miguel Cervantes");
-library_book.push(don_quixote);
+//dummy books to give a feel for the website 
 
-function add_book(){
-    const book_name_value=document.querySelector(".user_input>form>#book_name");
-    const author_name_value=document.querySelector(".user_input>form>#author_name");
-    
-    //adding elemnts to a newly created div
-    const new_book=document.createElement("div");
-    new_book.setAttribute("data-book_name",book_name_value.value);
-    const insert_img=document.createElement("img");
-    
-    //image
-    insert_img.setAttribute("src","img/icons8-book-100 (1).png")
-    new_book.appendChild(insert_img);
-    
-    //text content
-    const text_content=document.createElement("div");
-    text_content.setAttribute("class","text_material");
-    const book_name=document.createElement("div");
-    book_name.setAttribute("class","book_name");
-    book_name.textContent=book_name_value.value;
-    text_content.appendChild(book_name);
-    const author_name=document.createElement("div");
-    author_name.setAttribute("class","author_name");
-    author_name.textContent=`-${author_name_value.value}`;
-    text_content.appendChild(author_name);
-    new_book.appendChild(text_content);
-    container.insertBefore(new_book,add_card);
-
-    const icons=document.createElement("div");
-    icons.setAttribute("class","icons");
-
-    const read_group=document.createElement("div");
-    read_group.setAttribute("class","read_group");
-    
-    const read_check=document.createElement("input");
-    read_check.setAttribute("id","read");
-    read_check.setAttribute("name","read");
-    read_check.setAttribute("type","checkbox");
-    read_group.appendChild(read_check);
-    const read_label=document.createElement("label");
-    read_label.textContent="Read?";
-    read_label.setAttribute("for","read");
-    read_group.appendChild(read_label);
-    
-    icons.appendChild(read_group);
-    
-    const remove_button=document.createElement("button");
-    remove_button.setAttribute("class","remove"); 
-    remove_button.setAttribute("data-book_name",book_name_value.value);
-    const remove_button_element=document.createElement("img");
-    remove_button_element.setAttribute("src","img/icons8-remove-128.png")
-    
-    remove_button.appendChild(remove_button_element);
-
-    icons.appendChild(remove_button);
-
-    new_book.appendChild(icons);
+let lord_of_the_rings=new book("Lord of The Rings","JRR Tolkien",true,240);
+let chronicles_of_narnia=new book("Chronicles of Narnia","CS Lewis",false,320);
+let don_quixote=new book("Don Quixote","Miguel Cervantes",true,500);
+book_list.push(lord_of_the_rings);
+book_list.push(chronicles_of_narnia);
+book_list.push(don_quixote);
 
 
-    let temp=new Book(book_name_value.value,author_name_value.value);
-    library_book.push(temp);
-
-    author_name_value.value="";
-
-    book_name_value.value="";
-}
-
-
-function delete_book(book_ref){
-    
-    const del_book=document.querySelector(".container>div[data-book_name='"+book_ref+"']");
-    const book_container=document.querySelector(".container");
-    console.log(del_book);
-    for(let i=0;i<library_book.length;i++){
-        if(library_book[i].title===book_ref){
-            library_book.splice(i,1);
-        }
-    }
-    book_container.removeChild(del_book);
-    
-}
-
-const add_button=document.querySelector(".add_card>button");
+//setting up the add button functionality
 const dialog=document.querySelector(".user_input");
-const submit_dialogue=document.querySelector(".save_btn");
-const container=document.querySelector(".container");
-const add_card=document.querySelector(".add_card");
 
-let user_input;
-
-add_button.addEventListener('click',()=>{
+// dialog.style.top = ((window.innerHeight/2) - (dialog.offsetHeight/2))+'px';
+// dialog.style.left = ((window.innerWidth/2) - (dialog.offsetWidth/2))+'px';
+const add_btn=document.querySelector(".header>.add_card>button");
+add_btn.addEventListener("click",()=>{
+    
     dialog.showModal();
 });
 
-submit_dialogue.addEventListener("click",()=>{
-
-add_book();
-const remove_button=document.querySelectorAll(".remove");
-
-remove_button.forEach((a)=>{
-    a.addEventListener("click",()=>{
-        delete_book(a.getAttribute("data-book_name"));
-    })
+const submit_btn=document.querySelector(".user_input_form>.buttons>.save_btn");
+submit_btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(check()){
+        
+    add();
+    clear();
+    dialog.close();}
+    
 });
 
-
+const cancel_btn=document.querySelector(".user_input_form>.buttons>.cancel");
+cancel_btn.addEventListener("click",(e)=>{
+    clear();
+    e.preventDefault();
+    dialog.close();
+    
 })
-const remove_button=document.querySelectorAll(".remove");
 
-remove_button.forEach((a)=>{
-    a.addEventListener("click",()=>{
-        delete_book(a.getAttribute("data-book_name"));
-    })
-});
+render();
+}
 
-
+set_stage();
+console.log(book_list);
